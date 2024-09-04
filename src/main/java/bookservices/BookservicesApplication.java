ca9daa654a222ecdb844ffcfd896d807f6ae8227
viswa <viswavard>
2024-09-04 09:17:21 +0530

    vvv

---
+++ src/main/java/bookservices/BookservicesApplication.java
@@ -0,0 +1,13 @@
+package com.lms.bookservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lms/bookservices/BookservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.bookservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
